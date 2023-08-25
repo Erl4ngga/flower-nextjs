f9719f0189e7ce4e1a6ef1d07110607dfb1e9d3f
Erl4ngga <widoerlangga9@gmail.com>
2023-08-26 00:06:43 +0700

    add bannersection and productcard

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,12 @@
-import NextLink from "next/link";
-import { Link } from "@nextui-org/link";
-import { Snippet } from "@nextui-org/snippet";
-import { Code } from "@nextui-org/code";
-import { button as buttonStyles } from "@nextui-org/theme";
 import { siteConfig } from "@/config/site";
 import { title, subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/icons";
-import CardNewsletter from "@/components/card/cardnewsletter";
+import BannerSection from "@/components/banner/banner";
+import ProductCard from "@/components/product/product";
 
 export default function Home() {
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+      <BannerSection />
       <div className="inline-block max-w-lg text-center justify-center">
         <h1 className={title()}>Make&nbsp;</h1>
         <h1 className={title({ color: "violet" })}>beautiful&nbsp;</h1>
@@ -22,9 +18,7 @@ export default function Home() {
           Beautiful, fast and modern React UI library.
         </h2>
       </div>
-      <div className="my-32">
-        <CardNewsletter />
-      </div>
+      <ProductCard />
     </section>
   );
 }
