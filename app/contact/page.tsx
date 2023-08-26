561fb91324b8d5bb10c1d29901e7b874cf959ec9
Erl4ngga <widoerlangga9@gmail.com>
2023-08-26 18:07:24 +0700

    add contact form

---
+++ app/contact/page.tsx
@@ -0,0 +1,10 @@
+import ContactForm from "@/components/form/contact";
+import { title } from "@/components/primitives";
+
+export default function DocsPage() {
+  return (
+    <div>
+      <ContactForm />
+    </div>
+  );
+}
