997bda8f27d30b793d1cddd4d7ec69158f194e93
Erl4ngga <widoerlangga9@gmail.com>
2023-08-26 18:00:47 +0700

    add Contact form

--- config/site.ts
+++ config/site.ts
@@ -9,8 +9,8 @@ export const siteConfig = {
       href: "/",
     },
     {
-      label: "Docs",
-      href: "/docs",
+      label: "Contact",
+      href: "/contact",
     },
     {
       label: "Product",
