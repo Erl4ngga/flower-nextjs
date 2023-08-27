09260612dd847ab4262eb473e02ec27b386e002b
Erl4ngga <widoerlangga9@gmail.com>
2023-08-27 07:07:30 +0700

    fix title navbar  and router mobile

--- config/site.ts
+++ config/site.ts
@@ -27,36 +27,24 @@ export const siteConfig = {
   ],
   navMenuItems: [
     {
-      label: "Profile",
-      href: "/profile",
+      label: "Home",
+      href: "/",
     },
     {
-      label: "Dashboard",
-      href: "/dashboard",
+      label: "Contact",
+      href: "/contact",
     },
     {
-      label: "Projects",
-      href: "/projects",
+      label: "Product",
+      href: "/product",
     },
     {
-      label: "Team",
-      href: "/team",
+      label: "Blog",
+      href: "/blog",
     },
     {
-      label: "Calendar",
-      href: "/calendar",
-    },
-    {
-      label: "Settings",
-      href: "/settings",
-    },
-    {
-      label: "Help & Feedback",
-      href: "/help-feedback",
-    },
-    {
-      label: "Logout",
-      href: "/logout",
+      label: "About",
+      href: "/about",
     },
   ],
   links: {
