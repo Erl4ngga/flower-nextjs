ad76cf0eacd05fff828d7e59cc8a97426db20053
Erl4ngga <widoerlangga9@gmail.com>
2023-08-27 07:18:07 +0700

    fix banner

--- components/banner/banner.tsx
+++ components/banner/banner.tsx
@@ -1,6 +1,6 @@
 export default function BannerSection() {
   return (
-    <section className="bg-center bg-no-repeat bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/conference.jpg')] bg-gray-700 bg-blend-multiply my-10">
+    <section className="bg-center bg-no-repeat bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/conference.jpg')] bg-gray-700 bg-blend-multiply">
       <div className="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
         <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">
           We invest in the world’s potential
