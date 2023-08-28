72c0a62d6cecece485f0fa571605553723974ccf
Erl4ngga <widoerlangga9@gmail.com>
2023-08-28 11:41:55 +0700

    add plugins @tailwindcss/aspect-ratio

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,20 @@
-import {nextui} from '@nextui-org/theme'
+import { nextui } from "@nextui-org/theme";
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateRows: {
+        "[auto,auto,1fr]": "auto auto 1fr",
+      },
+    },
   },
   darkMode: "class",
-  plugins: [nextui()],
-}
+  plugins: [require("@tailwindcss/aspect-ratio"), nextui()],
+};
