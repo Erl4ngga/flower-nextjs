5d1130185d10a283b3ea6deabbcfd218b3844ce4
Erl4ngga <widoerlangga9@gmail.com>
2023-08-28 19:22:45 +0700

    add use state subject

--- components/form/contact.tsx
+++ components/form/contact.tsx
@@ -53,7 +53,6 @@ export default function ContactForm() {
             </label>
             <input
               type="text"
-              id="subject"
               className={`block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border ${
                 showWarning ? "border-red-500" : "border-gray-300"
               } shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light`}
