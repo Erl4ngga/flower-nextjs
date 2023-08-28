2f35c89fb49ed2000cac262f11fe39e1f5f82701
Erl4ngga <widoerlangga9@gmail.com>
2023-08-28 07:36:26 +0700

    add handle email

--- components/form/contact.tsx
+++ components/form/contact.tsx
@@ -1,4 +1,14 @@
+"use client";
+import React, { useState } from "react";
 export default function ContactForm() {
+  const [email, setEmail] = useState("");
+  const [showWarning, setShowWarning] = useState(false);
+
+  const handleEmailChange = (event: { target: { value: any } }) => {
+    const newEmail = event.target.value;
+    setEmail(newEmail);
+    setShowWarning(!newEmail.includes("@"));
+  };
   return (
     <section className="bg-white dark:bg-black">
       <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
@@ -17,10 +27,19 @@ export default function ContactForm() {
             <input
               type="email"
               id="email"
-              className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+              className={`shadow-sm bg-gray-50 border ${
+                showWarning ? "border-red-500" : "border-gray-300"
+              } text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light`}
               placeholder="name@flowbite.com"
+              value={email}
+              onChange={handleEmailChange}
               required
             />
+            {showWarning && (
+              <p className="text-red-500 text-sm mt-1">
+                Please enter a valid email address.
+              </p>
+            )}
           </div>
           <div>
             <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
