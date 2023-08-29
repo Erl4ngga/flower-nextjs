78f42609947bda249434978dfac3f5d8acae6733
Erl4ngga <widoerlangga9@gmail.com>
2023-08-30 00:04:38 +0700

    add cart product

---
+++ app/cart/page.tsx
@@ -0,0 +1,10 @@
+import CartProduct from "@/components/product/cartproduct";
+import ProductFeature from "@/components/product/productfeature";
+
+export default function AboutPage() {
+  return (
+    <div className="py-12 my-10">
+      <CartProduct />
+    </div>
+  );
+}
