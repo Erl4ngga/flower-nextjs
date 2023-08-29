adc1cfc0affd524f45a1c535d772dbd785262aed
Erl4ngga <widoerlangga9@gmail.com>
2023-08-29 13:44:16 +0700

    fix layout cart

--- app/cart/layout.tsx
+++ app/cart/layout.tsx
@@ -4,7 +4,7 @@ export default function AboutLayout({
   children: React.ReactNode;
 }) {
   return (
-    <section className="flex flex-col items-center justify-center gap-4">
+    <section>
       <div className="">{children}</div>
     </section>
   );
